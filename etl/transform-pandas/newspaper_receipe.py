30c4ea5dfb64c36ec4eb753bb2384c90a6e40611
Franco Manca <francomanca93@gmail.com>
2020-07-23 07:31:31 -0300

    Función para rellenar titulos vacios

--- etl/transform-pandas/newspaper_receipe.py
+++ etl/transform-pandas/newspaper_receipe.py
@@ -31,6 +31,7 @@ def main(filename):
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
 
     return df
 
@@ -114,6 +115,33 @@ def _extract_host(df):
     '''
     logger.info('Extracting host from urls')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+def _fill_missing_titles(df):
+    '''Función para rellenar titulos vacios
+    
+    Parameters
+    ----------
+    - df : dataframe
+        Recibe el dataset.
+    
+    Return 
+    -------
+    - df : dataframe
+        Devuelve el dataset con una columna adicional.
+    '''
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                      )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
     return df
 
